083c3c67cb5ba56718089043b657efec56f6054b
홍석현 <seokh1213@gmail.com>
2023-01-02 18:21:05 +0900

    feat: java로 변경

---
+++ src/main/java/com/paintingdiary/backend/PaintingDiaryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.paintingdiary.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintingDiaryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaintingDiaryBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paintingdiary/backend/PaintingDiaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paintingdiary.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintingDiaryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
