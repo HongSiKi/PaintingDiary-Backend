bf594410a4ac8cce8f8b2542b041f16f0ecd96eb
홍석현 <seokh1213@gmail.com>
2023-01-02 17:04:48 +0900

    feat: 초깃 설정

---
+++ src/main/kotlin/com/paintingdiary/paintingdiarybackend/PaintingDiaryBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.paintingdiary.paintingdiarybackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaintingDiaryBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<PaintingDiaryBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/paintingdiary/paintingdiarybackend/PaintingDiaryBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.paintingdiary.paintingdiarybackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaintingDiaryBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
